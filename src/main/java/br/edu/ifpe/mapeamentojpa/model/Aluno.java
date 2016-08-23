82a719f2771998402a3d511c067e7611e89d2e31
Tiago Lima <tiagorodrigodelima@hotmail.com>
2016-08-23 17:32:51 -0300

    Commit Inicial

---
+++ src/main/java/br/edu/ifpe/mapeamentojpa/model/Aluno.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.ifpe.mapeamentojpa.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Tiago Lima <tiagorodrigodelima@hotmail.com>
+ */
+@Entity
+public class Aluno implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    private String nome;
+    private String matricula;
+    private String cpf;
+    
+    @Temporal(TemporalType.DATE)
+    private Date dataNascimento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    
+}
